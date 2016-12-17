4cb1bf7802147e91b7bcf0d0f8436b1c719f9fd0
AleksZ13ru <a.zaykin@list.ru>
2016-12-17 12:33:58 +0300

    modify dept.models
    Добавление моделей в проект "Отдел"

--- dept/admin.py
+++ dept/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import People, Function, Holiday
+from .models import People, Function, Holiday, Schedule, Weekend, Change
 
 
 admin.site.register(Function)
 admin.site.register(People)
 admin.site.register(Holiday)
+admin.site.register(Change)
+admin.site.register(Schedule)
+admin.site.register(Weekend)
--- dept/models.py
+++ dept/models.py
@@ -11,7 +11,6 @@ class People(models.Model):
     dayofwork = models.DateField(blank=True, null=True)
     telefon = models.CharField(max_length=30, blank=True, null=True)
     address = models.TextField(blank=True, null=True)
-    #holiday = models.ForeignKey('Holiday', blank=True, null=True)
 
     def __str__(self):
         fio = '%s %s.%s. - %s' % (
@@ -57,3 +56,54 @@ class Holiday(models.Model):
     def __str__(self):
         return "%s %s - %s (%s)" % (People.fio(self.people),
                                     self.year, self.startday, self.typ)
+
+
+# График работы
+class Schedule(models.Model):
+    title = models.CharField(max_length=50)
+    struct = models.CharField(max_length=31)
+    startday = models.DateField()
+    starttime = models.TimeField()
+    stoptime = models.TimeField()
+
+    def __str__(self):
+        return "%s c %s по %s, (%s)" % (
+            self.title, self.starttime,
+            self.stoptime, self.struct)
+
+
+# Описание смен
+class Change(models.Model):
+    title = models.CharField(max_length=50)
+    starttime = models.TimeField()
+    stoptime = models.TimeField()
+
+    def __str__(self):
+        return self.title
+
+
+# Отгулы и выходные
+class Weekend(models.Model):
+    TYP_MOTIVE = (
+        (u'Служеб.', u'Служебное задание'),
+        (u'Семейн.', u'Семейные обстоятельства'),
+        (u'Другое ', u'Другое'),
+    )
+    TYP_CHOICES = (
+        (u'Выходной', u'Выходной'),
+        (u'Отгул', u'Отгул'),
+        (u'Без содержания', u'Без содержания'),
+    )
+    people = models.ForeignKey('People')
+    typ = models.CharField(max_length=16, choices=TYP_CHOICES)
+    motive = models.CharField(max_length=8, choices=TYP_MOTIVE)
+    datestart = models.DateField()
+    timestart = models.TimeField()
+    stoptime = models.TimeField()
+
+    def __str__(self):
+        return '%s был в: %s - %s числа по причине: %s  ' % (
+            People.fio(self.people),
+            self.typ,
+            self.datestart,
+            self.motive)
