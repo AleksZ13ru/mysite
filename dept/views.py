3bf2faf7a7d0570a2a7d074a24a8098c6f1ed259
zaykin_in_work <a.zaykin@list.ru>
2016-12-22 11:05:36 +0300

    Поправил форму calendar

--- dept/views.py
+++ dept/views.py
@@ -25,17 +25,13 @@ def dept_list(request):
 
 def dept_calendar(request):
     date = timezone.now()
-    #date.day = 1
-
     mycal = calendar.Calendar(firstweekday=0)
-    #mount = calendar.monthrange(timezone.now().year, timezone.now().month)
-    #mount_len = mount[1]
-    #mount_start_wek = mount[0]
     args = {}
-    day = []
-    for i in mycal.itermonthdates(timezone.now().year, timezone.now().month):
+    days = []
+    for i in mycal.itermonthdates(date.year, date.month):
         if i.month == date.month:
-            day.append(i)
-    args['day'] = day
+            days.append(i)
+    args['day'] = days
+    args['date'] = date  # current date
     args['schedules'] = Schedule.itermonthdates(timezone.now().year, timezone.now().month)
     return render(request, 'dept/dept_calendar.html', {'args': args})
