5017a5c55cfd59eebf7edf8b35375da37dee98ef
zaykin_in_work <a.zaykin@list.ru>
2017-01-19 15:38:44 +0300

    Celery настройка

--- si8parsing/task.py
+++ si8parsing/task.py
@@ -1,11 +1,16 @@
 # Create your tasks here
 from __future__ import absolute_import, unicode_literals
 from celery import shared_task
-
+from .code2 import findfile, parsing_si8
 
 @shared_task
 def add(x, y):
     return x + y
+
+
+@shared_task
+def find():
+    return findfile()
 
 
 @shared_task
