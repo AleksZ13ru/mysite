e585fdf2fc223744e87b182f4a2a56714aa6583f
AleksZ13ru <a.zaykin@list.ru>
2017-04-02 22:35:04 +0300

    Правка отображения админ модели dept

--- dept/admin.py
+++ dept/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
-from .models import People, Function, Holiday,\
+from .models import People, Function, Holiday, \
     Schedule, Weekend, Change, MicroSchedule
 
 
 class PeopleAdmin(admin.ModelAdmin):
     list_display = ('fio', 'function', 'schedule')
+
+
+class HolidayAdmin(admin.ModelAdmin):
+    list_display = ('people', 'year', 'typ', 'startday')
 
 
 class ChangeInline(admin.TabularInline):
@@ -17,9 +21,13 @@ class ScheduleAdmin(admin.ModelAdmin):
     inlines = [ChangeInline]
 
 
+class MicroScheduleAdmin(admin.ModelAdmin):
+    list_display = ('people', 'schedule', 'startday', 'lenght')
+
+
 admin.site.register(Function)
 admin.site.register(People, PeopleAdmin)
-admin.site.register(Holiday)
+admin.site.register(Holiday, HolidayAdmin)
 admin.site.register(Schedule, ScheduleAdmin)
 admin.site.register(Weekend)
-admin.site.register(MicroSchedule)
+admin.site.register(MicroSchedule, MicroScheduleAdmin)
