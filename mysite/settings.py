a32267072a552084761f8e80bcf110454638076d
AleksZ13ru <a.zaykin@list.ru>
2017-04-04 20:15:49 +0300

    filer настройка

--- mysite/settings.py
+++ mysite/settings.py
@@ -158,6 +158,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 FILER_IS_PUBLIC_DEFAULT = False
+FILER_ENABLE_PERMISSIONSвTrue = True
+
 FILER_STORAGES = {
     'public': {
         'main': {
